488ccdda8dbd88228c3e5e3fc1dabe3becdc9349
Ali <symeri@kth.se>
2017-02-19 14:16:42 +0100

    added map

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,6 +4,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { User } from '../models/user';
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class UserService {
