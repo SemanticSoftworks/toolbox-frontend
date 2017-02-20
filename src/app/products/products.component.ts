dd1f33db32f55d6a2ff39ff3d9299b15a59d9038
softish <lazar.h.klincov@live.se>
2017-02-20 16:12:06 +0100

    Drawing products and details on same page.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,12 +7,36 @@ import {AdService} from '../services/ad.service'
 
 @Component({
   selector: 'products-root',
+  //selector: 'app-root',
   templateUrl: './products.component.html',
+  /*
+  template: `
+  <h1>{{title}}</h1>
+    <ul class="ad">
+      <li *ngFor="let ad of ads"
+        [class.selected]="ad === selectedAd"
+        (click)="onSelect(ad)">
+        <span class="badge">{{ad.adId}}</span> {{ad.title}}
+      </li>
+    </ul>
+    <div *ngIf="selectedAd">
+      <h2>{{selectedAd.title}} details!</h2>
+      <div><label>id: </label>{{selectedAd.adId}}</div>
+      <!--
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedAd.title" placeholder="title"/>
+      </div>
+      -->
+    </div>
+  `,
+  */
   providers: [AdService],
-  styleUrls: ['./products.component.css'],
+  styleUrls: ['./products.component2.css'],
 })
 export class ProductsComponent {
   ads: Ad[];
+  selectedAd: Ad;
 
   constructor(private router: Router, private adService: AdService) { 
     this.adService.getAds().subscribe(ads => {
@@ -20,10 +44,16 @@ export class ProductsComponent {
       console.log(ads); 
       this.ads = ads;
     });
-
   }
-  title = 'app works';
+
+  onSelect(ad: Ad): void {
+    this.selectedAd = ad;
+  }
+
+  title = 'Products';
 }
+
+/*
 
 interface Ad {
   adId: number,
@@ -32,4 +62,14 @@ interface Ad {
   title: string,
   description: string,
   duration: string
+}
+*/
+
+export class Ad {
+  adId: number;
+  userId: number;
+  category: string; // 4 now
+  title: string;
+  description: string;
+  duration: string;
 }
