76af1d733ff6fff3cae2907346d6ec8b8c75fd47
carlitos84 <cgaldo@kth.se>
2017-02-20 13:53:44 +0100

    search component, bad return, undefined

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,20 +6,25 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {SearchService} from "../services/search.service";
 import {log} from "util";
+import {Headers, RequestOptions, Response, Http} from "@angular/http";
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import {Ad} from "../models/ad";
 
 @Component({
   selector: 'search-root',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
+  providers: [SearchService]
 })
 export class SearchComponent {
 
-  constructor(private router: Router, private searchService : SearchService) { }
+  list: Ad[];
+  constructor(private router: Router, private http:Http, private searchService : SearchService) {
 
-  getAllAds() {
-    log("Asdf");
-    this.searchService.getLatestAds();
-
+    this.searchService.getLatestAds().subscribe(x => {this.list = x});
+    console.log(this.list);
   }
+
   title = 'app wörks';
 }
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -4,25 +4,21 @@
 
 import {Injectable} from "@angular/core";
 import {Http, Response, Headers, RequestOptions} from "@angular/http";
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
 import {log} from "util";
+import {Ad} from "../models/ad";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class SearchService {
   constructor(private http: Http) {
   }
-  getLatestAds() {
-    console.log("Dsa");
-    return this.http.get('130.229.158.178:8090/ad/getads/0', this.jwt()).map((response: Response) => response.json());
-  }
-
-  // private helper methods
-
-  private jwt() {
-    // create authorization header with jwt token
-    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    if (currentUser && currentUser.token) {
-      let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
-      return new RequestOptions({ headers: headers });
-    }
+  getLatestAds(): Observable<Ad[]> {
+    console.log("Search request");
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get('http://130.229.128.245:8090/ad/search/testDescription').map((response: Response) => response.json());
   }
 }
