e34ec361024459b077f0eb0ffe14baf987bf40cd
Ali <symeri@kth.se>
2017-02-21 23:53:51 +0100

    edit profile without backendcall atm

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../services/user.service";
 import {User} from "../models/user";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'profile-root',
@@ -13,14 +14,27 @@ import {User} from "../models/user";
 
 export class ProfileComponent implements OnInit{
   currentUser: User;
+  model: any = {};
+  edit: boolean;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,private router: Router) {
     //Should not be in here if not logged in.
     this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
   ngOnInit() {
 
+  }
+
+  updateProfile() {
+    console.log("test update");
+    this.edit = false;
+    this.router.navigate(['/profile']);
+  }
+
+  toggleEdit() {
+    this.edit = true;
+    this.router.navigate(['/profile']);
   }
 
 }
