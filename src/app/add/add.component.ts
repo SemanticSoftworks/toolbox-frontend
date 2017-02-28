d146c6e4b7cd882b2c112b126bdc2a570c9d645d
carlitos84 <cgaldo@kth.se>
2017-02-28 15:30:53 +0100

    Ad create

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -3,6 +3,7 @@ import {Http, Headers, RequestOptions, Response} from "@angular/http";
 import {Router} from "@angular/router";
 import {Ad} from "../models/ad";
 import {User} from "../models/user";
+import {AdService} from "../services/ad.service";
 
 @Component({
   moduleId: module.id,
@@ -12,32 +13,18 @@ import {User} from "../models/user";
 
 export class AddComponent {
 
-  ad:Ad;
   currentUser: User;
 
-  constructor(private router: Router, private http:Http) {
-    //let ad2:Ad = {user: 1, title: "abc", description: "def", category: "test"};
+  constructor(private router: Router, private http:Http, private adService: AdService) {}
 
-  }
-
-  add(username: number, title: string, description: string, category: string) {
-    //Get logged in user and use its id in ad
+  add(username: number, category: number, title: string, description: string) {
+    //Get logged in user and use its id in ad, Not implemented
     //this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    console.log('in add...');
-    console.log(title + ' ' );
 
-    /*
-    this.ad.user = 1;
-    this.ad.title = title;
-    this.ad.description = description;
-    this.ad.category = category;
-*/
-    let ad2: Ad = ({adId: 3, user: 1, title: "abc", description: "def", category: "test", duration: null});
-
-    console.log(ad2.user.valueOf());
-
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-    return this.http.post('http://130.229.173.224:8090/ad/add', ad2, options).map((response: Response) => response.json());
+    let ad: Ad = ({adId: -1, userId: 1, title: title, description: description, category: category, date: null, photosId: null});
+    this.adService.createAd(ad).subscribe(ads => {
+      console.log("getting ads");
+      console.log(ads);
+    });
   }
 }
--- src/app/services/ad.service.ts
+++ src/app/services/ad.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Headers, RequestOptions, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {Ad} from "../models/ad";
 
 @Injectable()
 export class AdService {
@@ -16,5 +17,11 @@ export class AdService {
     getAd(ad: number) {
         return this.http.get('http://81.224.130.14:8090/ad/' + ad)
             .map(res => res.json());
+    }
+
+    createAd(ad :Ad) {
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+      return this.http.post('http://130.229.158.237:8090/ad/add', ad, options).map((response: Response) => response.json());
     }
 }
